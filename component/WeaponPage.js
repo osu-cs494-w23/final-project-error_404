f265fcd60e0fac296f743de43206e656ac831f4f
LyhongPeou <102566014+LyhongPeou@users.noreply.github.com>
2023-03-16 00:16:45 -0700

    Added properties and font

--- component/WeaponPage.js
+++ component/WeaponPage.js
@@ -20,21 +20,23 @@ const WeaponPage = () => {
     //console.log(error)
     const router = useRouter()
     return(
-        <Container>
+        <Container >
                 <h1 className={classes.weaponHeading}>List of Valorant weapons</h1>
-                {weapons.map((weapon) => (
-                    <>
-                    {/* <Link href={`${router.asPath}/${weapon.uuid}`}> */}
-                    <Link href={`${router.asPath}/${weapon.displayName}`}>
-                    <Card key={weapon.uuid} className={classes.weaponCard}>
-                        <Card.Img src={weapon.displayIcon} alt="Card image" className={classes.weaponImage}/>
-                        <Card.ImgOverlay  >
-                            <Card.Title style={{ color: 'white' }}>{weapon.displayName}</Card.Title>
-                        </Card.ImgOverlay>
-                    </Card>
-                    </Link> 
-                    </>
-                ))}
+                <div className="d-flex gap-4 flex-wrap mt-5">
+                    {weapons.map((weapon) => (
+                        <>
+                        {/* <Link href={`${router.asPath}/${weapon.uuid}`}> */}
+                        <Link href={`${router.asPath}/${weapon.displayName}`}>
+                        <Card key={weapon.uuid} className={classes.weaponCard}>
+                            <Card.Img src={weapon.displayIcon} alt="Card image" className={classes.weaponImage}/>
+                            <Card.ImgOverlay  >
+                                <Card.Title style={{ color: 'white' }}>{weapon.displayName}</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card>
+                        </Link>
+                        </>
+                    ))}
+                </div>
                 
         </Container>
     )
