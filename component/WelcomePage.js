28b2943aea72e16b6ffa56b28770e230b3be272b
LyhongPeou <102566014+LyhongPeou@users.noreply.github.com>
2023-03-22 03:11:34 -0700

    Search working and fix the background

--- component/WelcomePage.js
+++ component/WelcomePage.js
@@ -1,29 +1,41 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import classes from "./WelcomePage.module.css";
-import ReactRotatingText from "react-rotating-text";
 import Dropdown from "react-bootstrap/Dropdown";
 import useAgentsList from "./Hooks/useAgentsList";
 import useWeapons from "./Hooks/useWeapons";
+import Link from 'next/link'
+
 
 const WelcomePage = () => {
+  const [agents, loading, error] = useAgentsList();
 
-  const Agent = useAgentsList();
-
-  console.log(Agent);
- 
-  const Weapons = useWeapons();
-  console.log(Weapons);
+  const [Weapons, loading2, error2] = useWeapons();
 
   const [query, setQuery] = useState("");
 
+  const [filterAgent, setFilterAgent] = useState([]);
+  const [filterWeapon, setFilterWeapon] = useState([]);
+
   const queryHandler = (e) => {
+    setQuery(e.target.value);
 
-    setQuery(e.target.value)
-    
-  }
+    if (agents) {
+      setFilterAgent(
+        agents.data.filter((agent) =>
+          agent.displayName.toUpperCase().includes(query.toUpperCase())
+        )
+      );
+    }
 
-  console.log(query)
+    if (Weapons) {
+      setFilterWeapon(
+        Weapons.filter((weapon) =>
+          weapon.displayName.toUpperCase().includes(query.toUpperCase())
+        )
+      );
+    }
+  };
 
   return (
     <Container>
@@ -34,17 +46,37 @@ const WelcomePage = () => {
 
         <div className={classes.search_group}>
           <div>
-            <input placeholder="Search For Agent/Weapons" value={query} onChange={queryHandler}/>
+            <input
+              placeholder="Search For Agent/Weapons"
+              value={query}
+              onChange={queryHandler}
+            />
           </div>
 
           <div>
-            <Dropdown.Menu show className={classes.dropdown}>
-              <Dropdown.Header>Weapons</Dropdown.Header>
+            {query.length != 0 && (
+              <Dropdown.Menu show className={classes.dropdown}>
+                <Dropdown.Header>Weapons</Dropdown.Header>
+                {filterWeapon.map((weapon) => {
+                  return (
+                    <Dropdown.Item key={weapon.uuid} as={Link} href={`/weapons/${weapon.displayName}`}>
+                      {weapon.displayName}
+                    </Dropdown.Item>
+                  );
+                })}
 
-              <Dropdown.Divider />
-              <Dropdown.Header>Agent</Dropdown.Header>
-
-            </Dropdown.Menu>
+        
+                <Dropdown.Divider />
+                <Dropdown.Header>Agent</Dropdown.Header>
+                {filterAgent.map((agent) => {
+                  return (
+                    <Dropdown.Item key={agent.uuid} as={Link} href={`/agents?charName=${agent.displayName}`}> 
+                      {agent.displayName} 
+                    </Dropdown.Item>
+                  );
+                }) }
+              </Dropdown.Menu>
+            )}
           </div>
         </div>
       </div>
