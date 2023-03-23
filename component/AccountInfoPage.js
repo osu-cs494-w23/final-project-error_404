33a42c71bba593ae99bb4778ab95f8eac27ccc9a
ncamposp <camposn@oregonstate.edu>
2023-03-22 19:51:47 -0700

    Added hooks to account page. Todo: show data

--- component/AccountInfoPage.js
+++ component/AccountInfoPage.js
@@ -2,27 +2,34 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import useDB from "./Hooks/useDB";
 import { Container } from "react-bootstrap";
-
+import classes from "./AccountInfoPage.module.css";
+import getAccount from "./Hooks/getAccount";
+import getMMR from "./Hooks/getMMR";
 
 const AccountInfoPage = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  //const [email, setEmail ] = useState("")
-  // const [username, setUsername] = useState("")
-  // const [tagline, setTagline ] = useState("")
-  // const [gamename, setGamename] = useState("")
-
   let email = ""
-  
+
   if(session && status === "authenticated"){
     // setEmail( session.user.email)
     email = session.user.email
     
   }
+  // Hooks that grab data--------------------
   const [ players , loading , error ] = useDB(email)
-  console.log(players)
-  //console.log(email)
+  let playerInfo = null
+  let mmrInfo = null
+  if(players){
+    playerInfo = getAccount(players.gamename, players.tagline)
+    mmrInfo = getMMR(players.gamename, players.tagline)
+  }
+  console.log("Players is: ", players)
+  console.log("Playerinfo is: ", playerInfo)
+  console.log("mmrInfo is: ", mmrInfo)
+  //-------------------------------------------
+
   if (status === "loading") {
     return <p>Loading ...</p>;
   }
@@ -30,10 +37,17 @@ const AccountInfoPage = () => {
   if (status === `unauthenticated`) {
     router.push(`/`);
   }
-
+  
   return (
-    <Container>
-      <p> Welcome to the Account Page</p>
+    <Container className={classes.container}>
+      <h1> Welcome to the Account Page</h1>
+
+      <h3>Username: {players.name} </h3>
+      <h3>Email: {players.email}</h3>
+      <h3>Gamename: {players.gamename}#{players.tagline}</h3>
+      {/* <h3>Gamename: {players.gamename} | Tagline: {players.tagline}</h3> */}
+
+      <h2>My Game Info</h2>
     </Container>
     
   )
